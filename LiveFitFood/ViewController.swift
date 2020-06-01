2fb8793b187cb33d4f9af3ee72b5a5100adaeffb
Louise Chan <lbchan525@gmail.com>
2020-06-01 00:10:32 -0400

    Initial Commit

---
+++ LiveFitFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiveFitFood
+//
+//  Created by Louise Chan on 2020-06-01.
+//  Copyright © 2020 Louise Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
